374cb51d32c6a3d96b6c0192b2b2a59db87ec7ba
xurxodev <xurxosf@gmail.com>
2023-10-31 16:22:02 +0100

    Add tests for id object value

--- src/test/kotlin/domain/valueObjects/IdShould.kt
+++ src/test/kotlin/domain/valueObjects/IdShould.kt
@@ -1,5 +1,6 @@
 package domain.valueObjects
 
+import domain.types.ValidationErrorKey
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -22,6 +23,58 @@ class IdShould {
         idResult.fold(
             { _ -> fail("Should be success") },
             { id -> assertEquals(id.value, existedId.value) }
-        );
+        )
+    }
+
+    @Test
+    fun `should return Id cannot be blank error if value argument is empty`() {
+        val idResult = Id.createExisted("")
+
+        idResult.fold(
+            { errors ->
+                assertEquals(1,errors.size)
+                assertEquals(ValidationErrorKey.FIELD_CANNOT_BE_BLANK, errors[0])
+            },
+            { _ -> fail("should be fail") }
+        )
+    }
+
+    @Test
+    fun `should return InvalidId error if value argument is invalid`() {
+        val idResult = Id.createExisted("wrong id")
+
+        idResult.fold(
+            { errors ->
+                assertEquals(1,errors.size)
+                assertEquals(ValidationErrorKey.INVALID_FIELD, errors[0])
+            },
+            { _ -> fail("should be fail") }
+        )
+    }
+
+    @Test
+    fun `should return InvalidId error if value argument starts with a number`() {
+        val idResult = Id.createExisted("0kWynlWMjJR")
+
+        idResult.fold(
+            { errors ->
+                assertEquals(1,errors.size)
+                assertEquals(ValidationErrorKey.INVALID_FIELD, errors[0])
+            },
+            { _ -> fail("should be fail") }
+        )
+    }
+
+    @Test
+    fun `should return InvalidId error if value argument contains non-alphanumeric characters`() {
+        val idResult = Id.createExisted("AkWy_lWMjJR")
+
+        idResult.fold(
+            { errors ->
+                assertEquals(1,errors.size)
+                assertEquals(ValidationErrorKey.INVALID_FIELD, errors[0])
+            },
+            { _ -> fail("should be fail") }
+        )
     }
 }
